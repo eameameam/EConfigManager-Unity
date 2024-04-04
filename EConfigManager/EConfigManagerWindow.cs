1c48f3ae99967dc5b4f41fbf7a1bc006639fdfa0
eameameam <eam.motion@gmail.com>
2024-04-05 01:24:19 +0200

    Update EConfigManagerWindow.cs

--- EConfigManager/EConfigManagerWindow.cs
+++ EConfigManager/EConfigManagerWindow.cs
@@ -2,6 +2,7 @@ using System.Linq;
 using UnityEditor;
 using UnityEngine;
 using System.Collections.Generic;
+using System;
 
 public class EConfigManagerWindow : EditorWindow
 {
@@ -41,134 +42,153 @@ public class EConfigManagerWindow : EditorWindow
 
     void OnGUI()
     {
-        GUILayout.Space(10);
-
-        GUILayout.BeginHorizontal();
-
-        GUILayout.BeginVertical();
-        GUILayout.Label("EConfig Manager", EditorStyles.largeLabel);
-        GUILayout.Label("Firstly set your configs by pressing SET button,\nit will open a window that you can set your configs.", EditorStyles.miniLabel);
-        GUILayout.EndVertical();
-
-        GUILayout.FlexibleSpace();
-        
-        GUI.backgroundColor = new Color(0.6f, 0.2f, 0.2f);
-        if (GUILayout.Button("SET", GUILayout.Height(20), GUILayout.Width(40)))
+        try
         {
-            ESelectConfigsWindow.ShowWindow();
-        }
-        GUI.backgroundColor = Color.white;
-        
-        GUILayout.Space(10);
+            GUILayout.Space(10);
 
-        GUILayout.EndHorizontal();
+            GUILayout.BeginHorizontal();
 
-        GUILayout.BeginHorizontal();
+            GUILayout.BeginVertical();
+            GUILayout.Label("EConfig Manager", EditorStyles.largeLabel);
+            GUILayout.Label("Firstly set your configs by pressing SET button,\nit will open a window that you can set your configs.", EditorStyles.miniLabel);
+            GUILayout.EndVertical();
 
-        GUILayout.FlexibleSpace();
-
-        GUILayout.BeginVertical(GUILayout.Width(30), GUILayout.Height(30));
-        GUILayout.Label("Backup", EditorStyles.whiteLabel);
-        if (GUILayout.Button("Save"))
-        {
-            if (EditorUtility.DisplayDialog("Save Backup", "Are you sure you want to save the backup?", "Save", "Cancel"))
+            GUILayout.FlexibleSpace();
+            
+            GUI.backgroundColor = new Color(0.6f, 0.2f, 0.2f);
+            if (GUILayout.Button("SET", GUILayout.Height(20), GUILayout.Width(40)))
             {
-                BackupManager.SaveBackup();
-                Debug.Log("Backup saved.");
+                ESelectConfigsWindow.ShowWindow();
             }
-        }
-        if (GUILayout.Button("Restore"))
-        {
-            if (EditorUtility.DisplayDialog("Restore Backup for all configs", "Are you sure you want to restore the backup for all configs in the list?", "Restore", "Cancel"))
+            GUI.backgroundColor = Color.white;
+            
+            GUILayout.Space(10);
+
+            GUILayout.EndHorizontal();
+
+            GUILayout.BeginHorizontal();
+
+            GUILayout.FlexibleSpace();
+
+            GUILayout.BeginVertical(GUILayout.Width(30), GUILayout.Height(30));
+            GUILayout.Label("Backup", EditorStyles.whiteLabel);
+            if (GUILayout.Button("Save"))
             {
-                BackupManager.RestoreBackup();
-                Debug.Log("Backup loaded.");
-            }
-        }
-        GUILayout.EndVertical();
-
-        GUILayout.Space(10); 
-
-        GUILayout.EndHorizontal();
-
-        GUILayout.Label("Configs", EditorStyles.helpBox);
-
-        if (!(_eConfigHolder.configs.Count > 0))
-        {
-            GUILayout.Space(20);
-
-            GUILayout.Label("No Configs Found", EditorStyles.centeredGreyMiniLabel);
-            return;
-        }
-
-        EConfigGUI.DrawSortOptions(ref _currentSortType, ref _ascending, ref _grouping,() => SortConfigs());
-
-        if (!_configsSorted)
-        {
-            SortConfigs();
-        }
-
-        _scrollPosition = EditorGUILayout.BeginScrollView(_scrollPosition);
-        bool wasGrouping = _grouping;
-        if (_eConfigHolder.configs.Count > 0)
-        {
-            EditorGUI.indentLevel = 0;
-            Color originalBackgroundColor = GUI.backgroundColor;
-
-            if (_grouping)
-            {
-                var groupedConfigs = ConfigGrouping.GroupConfigs(_eConfigHolder.configs, _currentSortType);
-                foreach (var groupKey in groupedConfigs.Keys)
+                if (EditorUtility.DisplayDialog("Save Backup", "Are you sure you want to save the backup?", "Save", "Cancel"))
                 {
-                    bool foldout = _groupFoldouts.TryGetValue(groupKey, out bool currentFoldoutState) ? currentFoldoutState : false;
-                    foldout = EditorGUILayout.Foldout(foldout, groupKey, true);
-                    _groupFoldouts[groupKey] = foldout;
-
-                    if (foldout)
-                    {
-                        EditorGUI.indentLevel++;
-                        
-                        var backgroundColor = GUI.backgroundColor;
-                        GUI.backgroundColor = Color.gray;
-
-                        foreach (var config in groupedConfigs[groupKey])
-                        {
-                            if (config != null)
-                            {
-                                SerializedObject serializedConfig = _serializedConfigs[config];
-                                EditorGUILayout.BeginVertical("box");
-                                EConfigGUI.DrawProperties(serializedConfig, config, _foldouts);
-                                EditorGUILayout.EndVertical();
-                            }
-                        }
-                        
-                        EditorGUI.indentLevel--;
-                        GUI.backgroundColor = backgroundColor;
-                    }
+                    BackupManager.SaveBackup();
+                    Debug.Log("Backup saved.");
                 }
             }
-            else
+            if (GUILayout.Button("Restore"))
             {
-                foreach (var config in _eConfigHolder.configs)
+                if (EditorUtility.DisplayDialog("Restore Backup for all configs", "Are you sure you want to restore the backup for all configs in the list?", "Restore", "Cancel"))
                 {
-                    if (config != null)
-                    {
-                        SerializedObject serializedConfig = _serializedConfigs[config];
-                        EditorGUILayout.BeginVertical("box");
-                        EConfigGUI.DrawProperties(serializedConfig, config, _foldouts);
-                        EditorGUILayout.EndVertical();
-                    }
+                    BackupManager.RestoreBackup();
+                    Debug.Log("Backup loaded.");
                 }
             }
+            GUILayout.EndVertical();
 
-            if (wasGrouping != _grouping || EditorGUI.indentLevel != 0 || originalBackgroundColor != GUI.backgroundColor)
+            GUILayout.Space(10); 
+
+            GUILayout.EndHorizontal();
+
+            GUILayout.Label("Configs", EditorStyles.helpBox);
+
+            if (!(_eConfigHolder.configs.Count > 0))
             {
-                Debug.LogError("GUI state has changed. Resetting...");
+                GUILayout.Space(20);
+
+                GUILayout.Label("No Configs Found", EditorStyles.centeredGreyMiniLabel);
+                return;
+            }
+
+            EConfigGUI.DrawSortOptions(ref _currentSortType, ref _ascending, ref _grouping,() => SortConfigs());
+
+            if (!_configsSorted)
+            {
+                SortConfigs();
+            }
+
+            _scrollPosition = EditorGUILayout.BeginScrollView(_scrollPosition);
+            bool wasGrouping = _grouping;
+            if (_eConfigHolder.configs.Count > 0)
+            {
                 EditorGUI.indentLevel = 0;
-                GUI.backgroundColor = originalBackgroundColor;
+                Color originalBackgroundColor = GUI.backgroundColor;
+
+                if (_grouping)
+                {
+                    var groupedConfigs = ConfigGrouping.GroupConfigs(_eConfigHolder.configs, _currentSortType);
+                    foreach (var groupKey in groupedConfigs.Keys)
+                    {
+                        bool foldout = _groupFoldouts.TryGetValue(groupKey, out bool currentFoldoutState) ? currentFoldoutState : false;
+                        foldout = EditorGUILayout.Foldout(foldout, groupKey, true);
+                        _groupFoldouts[groupKey] = foldout;
+
+                        if (foldout)
+                        {
+                            EditorGUI.indentLevel++;
+                            
+                            var backgroundColor = GUI.backgroundColor;
+                            GUI.backgroundColor = Color.gray;
+
+                            foreach (var config in groupedConfigs[groupKey])
+                            {
+                                if (config != null)
+                                {
+                                    SerializedObject serializedConfig = _serializedConfigs[config];
+                                    EditorGUILayout.BeginVertical("box");
+                                    EConfigGUI.DrawProperties(serializedConfig, config, _foldouts);
+                                    EditorGUILayout.EndVertical();
+                                }
+                            }
+                            
+                            EditorGUI.indentLevel--;
+                            GUI.backgroundColor = backgroundColor;
+                        }
+                    }
+                }
+                else
+                {
+                    foreach (var config in _eConfigHolder.configs)
+                    {
+                        if (config != null)
+                        {
+                            SerializedObject serializedConfig = _serializedConfigs[config];
+                            EditorGUILayout.BeginVertical("box");
+                            EConfigGUI.DrawProperties(serializedConfig, config, _foldouts);
+                            EditorGUILayout.EndVertical();
+                        }
+                    }
+                }
+
+                if (wasGrouping != _grouping || EditorGUI.indentLevel != 0 || originalBackgroundColor != GUI.backgroundColor)
+                {
+                    Debug.LogError("GUI state has changed. Resetting...");
+                    EditorGUI.indentLevel = 0;
+                    GUI.backgroundColor = originalBackgroundColor;
+                }
+            }
+            EditorGUILayout.EndScrollView();
+
+            if (GUI.changed)
+            {
+                Debug.Log("GUI changed, requesting repaint");
+                EditorGUIUtility.ExitGUI();
             }
         }
-        EditorGUILayout.EndScrollView();
+        catch (Exception ex)
+        {
+            Debug.LogError($"Rendering error: {ex}");
+            EditorGUIUtility.ExitGUI();
+        }
+
+        if (GUI.changed)
+        {
+            Repaint();
+        }
     }
 
     void LoadConfigs()
